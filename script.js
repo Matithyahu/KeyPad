1bf1d7e69cf4c8d9c5e56e58f1ac729d659c0b6a
Matithyahu <matthew@metzger.cc>
2017-07-30 00:00:00 -0500

    kVVhtml

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var keyValue = "";
 
 var keyValueViewer = document.querySelector("p");
-keyValueViewer.innerHTML = "Enter six numbers"
+keyValueViewer.innerHTML = "Enter six numbers";
+keyValueViewer.innerHTML = kVVhtml;
 
 numbers.forEach(function(n) {
   var element = document.getElementById(n.toString());
